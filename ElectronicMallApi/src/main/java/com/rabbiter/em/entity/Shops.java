0bbffe21634bf30fbdf11c7430f009ef045f9d66
huayuhong <1492178288@qq.com>
2025-04-24 18:08:52 +0800

    shops实体类

---
+++ ElectronicMallApi/src/main/java/com/rabbiter/em/entity/Shops.java
@@ -0,0 +1,107 @@
+package com.rabbiter.em.entity;
+
+
+import com.baomidou.mybatisplus.annotation.TableName;
+
+@TableName("shops")
+public class Shops {
+
+    private Integer id;
+
+    private String floor;
+    private String shopName;
+
+    private Integer x;
+
+    private Integer y;
+
+    private Integer width;
+    private Integer height;
+
+    private String shape;
+
+    private String description;
+
+    private String contactInfo;
+
+    // Getters 和 Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+}    
